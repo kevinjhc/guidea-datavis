30cf8ce728c9a3b27853248a54ad5ccfba5e1f77
Kevin Chang <kchang@octolabs.io>
2021-01-21 10:01:26 -0500

    Updated qual palettes

--- src/components/charts/ChartColors.js
+++ src/components/charts/ChartColors.js
@@ -1,17 +1,17 @@
 const colors = {
   lightTheme: {
-    qualitative: ["#2E96FF", "#5A068E", "#8D0C6A", "#9565FE", "#4B7DA4", "#007D79", "#00A862", "#67A503", "#1E43EB", "#FF2EEA", "#FF576A", "#780C12"],
+    qualitative: ["#2E96FF", "#5A068E", "#780C12", "#007D79", "#1E43EB", "#00A862", "#67A503", "#EE51DB", "#FF576A", "#000053", "#6F6FFF", "#8D0C6A", "#4B7DA4" ],
     // sequentialPurple: ["#580450", "#700666", "#88077C", "#A00892", "#AE2DA2", "#BC52B3", "#CB77C3", "#D99CD3", "#E7C1E4", "#F5E6F4"],
     sequentialBlue: ["#0D1F72", "#082194", "#1433C3", "#1E43EB", "#4B69EF", "#6D85F2", "#9AAAF6", "#BBC7F9", "#D2D9FB", "#E8ECFD"],
     diverging: ["#1433C2", "#1E43EB", "#5672F0", "#8398F4", "#B0BDF8", "#D2D9FB", "#E8ECFD", "#EEDBE9", "#E3C2DA", "#D19EC3", "#C079AD", "#AF5597", "#8D0C6A", "#710955"],
-    semantic: ["#F41824", "#F05400", "#FFCD00", "#1E43EB"]
+    semantic: ["#F41824", "#FF9605", "#FFCD00", "#1E43EB"]
   },
   darkTheme: {
-    qualitative: ["#40A0FF", "#4D2CA2", "#B80F8B", "#9565FE", "#4B7DA4", "#007D79", "#00A862", "#81CE04", "#1E43EB", "#FF2EEA", "#FF576A", "#B50D15"],
+    qualitative: ["#40A0FF", "#BD5DFF", "#007D70", "#2C4FED", "#00A862", "#81CE04", "#B80F8B", "#FF87FF", "#FF576A", "#00C3CC", "#6F6FFF", "#4B7DA4", "#B50D15"],
     // sequentialPurple: ["#580450", "#700666", "#88077C", "#A00892", "#AE2DA2", "#BC52B3", "#CB77C3", "#D99CD3", "#E7C1E4", "#F5E6F4"],
     sequentialBlue: ["#0D1F72", "#082194", "#1433C2", "#2C4FED", "#5672F1", "#768DF3", "#A0B0F7", "#C0CAFA", "#D5DCFB", "#EAEDFD"],
     diverging: ["#1433C2", "#1E43EB", "#617BF1", "#8B9EF5", "#B5C1F9", "#D5DCFB", "#EAEDFD", "#F4DBEE", "#EDC3E2", "#E39FD1", "#D87BBF", "#CD57AE", "#B80F8B", "#8D0C6A"],
-    semantic: ["#F41824", "#FF831D", "#FFCD00", "#2C4FED"]
+    semantic: ["#F41824", "#FF9605", "#FFCD00", "#2C4FED"]
   }
 }
 
